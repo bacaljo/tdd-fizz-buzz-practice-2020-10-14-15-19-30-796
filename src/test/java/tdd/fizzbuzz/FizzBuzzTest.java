e8337c57b174210eacc880339e7b2354f7490bc1
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 14:01:46 +0800

    [FizzBuzz][John Emmanuel] test: add a setUp method for FizzBuzz initialization and create a test for FizzBuzz.countOff to return 1 given 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,26 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
+
+    public FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    void should_return_1_when_count_off_given_1() {
+        // given
+        int orderNumber = 1;
+
+        // when
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        // then
+        assert("1").equals(actual);
+    }
 }
