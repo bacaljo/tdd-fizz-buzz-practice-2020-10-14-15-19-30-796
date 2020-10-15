49f82b90cdaa87106f6c0422a099af83cf31e021
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 15:13:00 +0800

    [FizzBuzz][John Emmanuel] refactor: simplify logic of countOff

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,33 +5,23 @@ public class FizzBuzz {
     private final String FIZZ = "Fizz";
     private final String BUZZ = "Buzz";
     private final String WHIZZ = "Whizz";
-    private final String FIZZBUZZ = "FizzBuzz";
-    private final String FIZZWHIZZ = "FizzWhizz";
-    private final String BUZZWHIZZ = "BuzzWhizz";
-    private final String FIZZBUZZWHIZZ = "FizzBuzzWhizz";
 
     public String countOff(int number) {
-        boolean isAMultipleOf3 = isMultipleOfNumber(number, 3);
-        boolean isAMultipleOf5 = isMultipleOfNumber(number, 5);
-        boolean isAMultipleOf7 = isMultipleOfNumber(number, 7);
+        StringBuilder stringBuilder = new StringBuilder();
 
-        if (isAMultipleOf3 && isAMultipleOf5 && isAMultipleOf7) {
-            return FIZZBUZZWHIZZ;
-        } else if (isAMultipleOf3 && isAMultipleOf5 && !isAMultipleOf7) {
-            return FIZZBUZZ;
-        } else if (isAMultipleOf3 && !isAMultipleOf5 && isAMultipleOf7) {
-            return FIZZWHIZZ;
-        } else if (!isAMultipleOf3 && isAMultipleOf5 && isAMultipleOf7) {
-            return BUZZWHIZZ;
-        } else if (isAMultipleOf3) {
-            return FIZZ;
-        } else if (isAMultipleOf5) {
-            return BUZZ;
-        } else if (isAMultipleOf7) {
-            return WHIZZ;
-        } else {
-            return Integer.toString(number);
+        if (isMultipleOfNumber(number, 3)) {
+            stringBuilder.append(FIZZ);
         }
+        if (isMultipleOfNumber(number, 5)) {
+            stringBuilder.append(BUZZ);
+        }
+        if (isMultipleOfNumber(number, 7)) {
+            stringBuilder.append(WHIZZ);
+        }
+
+        return stringBuilder.toString().isEmpty()
+                ? Integer.toString(number)
+                : stringBuilder.toString();
     }
 
     private boolean isMultipleOfNumber(int number, int modulus) {
