d44cc04e5c86d59ec98b9b68790462da3045eb62
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 14:06:24 +0800

    [FizzBuzz][John Emmanuel] feature: provide the correct logic to countOff to return Fizz when given 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,9 @@ public class FizzBuzz {
     public String countOff(int number) {
         if (number == 1)
             return "1";
+        else if (number == 3) {
+            return "Fizz";
+        }
 
         return null;
     }
