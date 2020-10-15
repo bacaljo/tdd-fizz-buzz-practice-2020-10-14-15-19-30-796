23ab9e9c8d768afba167c0aa7026061e47b7ec80
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 14:33:23 +0800

    [FizzBuzz][John Emmanuel] refactor: revise FizzBuzz.countOff to return Buzz given multiples of 5 and create a constant for Buzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,15 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     private final String FIZZ = "Fizz";
+    private final String BUZZ = "Buzz";
 
     public String countOff(int number) {
         if (number == 1)
             return "1";
         else if (isMultipleOfNumber(number, 3)) {
             return FIZZ;
-        } else if (number == 5) {
-            return "Buzz";
+        } else if (isMultipleOfNumber(number, 5)) {
+            return BUZZ;
         } else if (number == 7) {
             return "Whizz";
         }
