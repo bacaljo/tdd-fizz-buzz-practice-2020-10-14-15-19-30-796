a3f1fed5e65f5e19f4b78a9607c93ebdecc438d6
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 14:44:26 +0800

    [FizzBuzz][John Emmanuel] refactor: revise FizzBuzz.countOff to return FizzBuzz given multiples of 3 and 5 but not a multiple of 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,15 +5,22 @@ public class FizzBuzz {
     private final String FIZZ = "Fizz";
     private final String BUZZ = "Buzz";
     private final String WHIZZ = "Whizz";
+    private final String FIZZBUZZ = "FizzBuzz";
 
     public String countOff(int number) {
+        boolean isAMultipleOf3 = isMultipleOfNumber(number, 3);
+        boolean isAMultipleOf5 = isMultipleOfNumber(number, 5);
+        boolean isAMultipleOf7 = isMultipleOfNumber(number, 7);
+
         if (number == 1)
             return "1";
-        else if (isMultipleOfNumber(number, 3)) {
+        else if (isAMultipleOf3 && isAMultipleOf5 && !isAMultipleOf7) {
+            return FIZZBUZZ;
+        } else if (isAMultipleOf3) {
             return FIZZ;
-        } else if (isMultipleOfNumber(number, 5)) {
+        } else if (isAMultipleOf5) {
             return BUZZ;
-        } else if (isMultipleOfNumber(number, 7)) {
+        } else if (isAMultipleOf7) {
             return WHIZZ;
         }
 
